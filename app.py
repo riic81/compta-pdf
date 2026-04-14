c06c068d92825b1edd417fb75ed97e7f756759b8
riic81 <102254716+riic81@users.noreply.github.com>
2026-04-14 19:17:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,11 +62,12 @@ if uploaded_files:
                     text += page.extract_text() + "\n"
                 results.append(extract_data(text))
 df["Date_obj"] = pd.to_datetime(df["Date_obj"], errors="coerce")
-df["Mois"] = df["Date_obj"].dt.strftime("%Y-%m")
+
         df = pd.DataFrame(results)
         df = df[df["Date_obj"].notna()]
         df = df.sort_values("Date_obj")
-        
+        df["Date_obj"] = pd.to_datetime(df["Date_obj"], errors="coerce")
+df["Mois"] = df["Date_obj"].dt.strftime("%Y-%m")
 
         st.dataframe(df)
 
